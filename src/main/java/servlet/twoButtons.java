215627b5f4fcce7423d24dc9d6d2de7f0bd33140
marisamills99 <marisamills99@gmail.com>
2021-02-24 11:33:35 -0500

    buttons added url pattern

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -26,7 +26,7 @@ import javax.servlet.http.*;
 //              Fields are filled from the parameters.
 // private void PrintTail (PrintWriter out) --> Prints the HTML bottom
 //***********************************************************************
-
+@WebServlet(name = "ButtonsServlet", urlPatterns = {"/twoButtons"})
 public class twoButtons extends HttpServlet
 {
 
